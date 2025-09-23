8e503211e3fdb3dd120721195dd432ac7303c695
Shivangi Singh <Shivangisingh212005@gmail.com>
2025-09-23 15:13:36 +0530

    Create exp1.cpp

---
+++ exp1.cpp
@@ -0,0 +1,24 @@
+//Shivangi Singh
+//24070123148
+//B3
+#include<iostream>
+using namespace std;
+class Node{
+  public:
+  int val;
+  Node* next;
+  //constructor to initialise node
+  Node (int data)
+  {
+  val=data;
+  next=NULL;
+  }
+  };
+  int  main(){
+      Node *n= new Node(1);
+      cout<<n->val<<""<<n->next<<endl;
+      return 0;
+  }
+/*Output
+10
+*/
